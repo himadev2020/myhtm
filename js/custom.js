82f440a02239663283ef3906359c11e1865f2b56
Tony <cem.tony@gmail.com>
2020-11-25 15:49:30 +0530

    added files

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+
+$(".pr-one").owlCarousel({
+  margin:10,
+  loop:true,
+  dots: true,
+  nav: false,
+  items: 1
+});    
+ $("input[type='number']").inputSpinner()
---
+++ single_product/js/custom.js
@@ -0,0 +1,55 @@
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 15,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  centerMode: false,
+  focusOnSelect: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: false,
+        centerPadding: '40px',
+        slidesToShow: 10
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: false,
+        centerPadding: '40px',
+        slidesToShow: 5
+      }
+    }
+  ]
+});
+
+
+
+ $('.prdslider-main').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.prd-thumb'
+});
+$('.prd-thumb').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.prdslider-main',
+  dots: false,
+  centerMode: false,
+  focusOnSelect: true
+});
+
+ $("input[type='number']").inputSpinner()
